ee9295f46259c4c70eb3b4173a1321e3b11bb3b6
Dima <dima92.mvs@gmail.com>
2017-02-06 10:57:47 +0200

    Delete <br>

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 
 get_header(); ?>
 
-	<br><br>
 	<div class="align-center">Set a regular page ( WP Admin > Pages ) to be the homepage ( WP Admin > Settings > Reading )</div>
 
 <?php get_footer(); ?>
